d40f1cbebc2e6307ca9c9973a38fc58747282728
partha chakraborty <partha chakraborty>
2016-09-16 21:39:36 +0600

    Ready for submit

--- Offline 01/OFFLINE 01(COMPILER).cpp
+++ Offline 01/OFFLINE 01(COMPILER).cpp
@@ -1,18 +1,14 @@
 #include<cstdio>
 #include<string>
 #include<iostream>
-
 #include<sstream>
 #include <fstream>
 #include <cstdlib>
-
-
 #define NULL_VALUE 0
 using namespace std;
 
 class SymbolInfo
 {
-
 public:
     SymbolInfo(string type,string  name)
     {
@@ -20,8 +16,6 @@ public:
         this->name=name;
         this->next= NULL_VALUE;
     }
-
-
     string getType()
     {
         return type;
@@ -43,12 +37,7 @@ private:
     string type;
     string name;
     SymbolInfo *next;
-
-
-
-
 };
-
 class position
 {
 private:
@@ -93,18 +82,14 @@ private:
         unsigned long hash = 123;
         int c;
         const char*st=st1.c_str();
-
-
         while (c = *st++)
         {
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-         }
-
+        }
         return (hash%maxSize);
     }
-
 public:
-    SymbolTable(int maxSize)
+    SymbolTable(int maxSize)// will have   maxSize number of row
     {
         this->maxSize=maxSize;
 
@@ -113,12 +98,25 @@ public:
         {
             *(table+i)=NULL_VALUE;
         }
-
-
     }
-    SymbolTable()
+    SymbolTable()// Used as size  is taken later
     {
 
+    }
+     ~SymbolTable()
+    {
+        SymbolInfo *temp,*temp1;
+        for(int i=0;i<maxSize;i++)
+        {
+           temp=table[i];
+            for(;temp!=NULL_VALUE;)
+            {
+               temp1=temp;
+               temp=temp->getNext();
+               delete(temp1);
+            }
+        }
+        cout<<"destructed"<<endl;
     }
     void setSize(int maxSize)
     {
@@ -128,16 +126,16 @@ public:
         table= new SymbolInfo*[maxSize];
         for(int i=0;i<maxSize;i++)
         {
-            *(table+i)=NULL_VALUE;
+            *(table+i)=NULL_VALUE;//necessary to find the end
         }
     }
     position  insert(SymbolInfo* input)
     {
         position pos_temp=(position)lookUp(input->getName());
-        if(pos_temp.getI()==-1)
+        if(pos_temp.getI()==-1)// checking is there another entry like this
         {
-            int pos = hash(input->getName());
-            if (table[pos] == NULL_VALUE) {
+            int pos = hash(input->getName());//getting hash position
+            if (table[pos] == NULL_VALUE) {//is it the first entry of that row
                 table[pos] = (input);
                 position cur(pos, 0);
                 return cur;
@@ -155,11 +153,10 @@ public:
         }
         else
         {
-            pos_temp.setI(-1);
+            pos_temp.setI(-1);//it's a duplicate entry
             pos_temp.setJ(-1);
             return  pos_temp;
         }
-
     }
     position lookUp(string st)
     {
@@ -172,21 +169,16 @@ public:
             temp_name=temp->getName();
             if(temp_name.compare(st.c_str())==0)
             {
-
                 position cur(pos,i);
                 cur.setType(temp->getType());
                 return cur;
-
             }
             else
             {
                 temp = temp->getNext();
             }
-
-
-
         }
-        position cur(-1,-1);
+        position cur(-1,-1);// as it is a duplicate entry
         return cur;
     }
     void print()
@@ -208,15 +200,14 @@ public:
         }
         return;
     }
-
     position Delete(string  st)
     {
         position cur=lookUp(st);
-        if(cur.getI()==-1)
+        if(cur.getI()==-1)// Checking the value is it exists
         {
             return cur;
         }
-        SymbolInfo* now,*prev;
+        SymbolInfo* now,*prev;// two pointer needed as it is a singly linked list
         now=prev=table[cur.getI()];
         for(int j=0;j!=cur.getJ();j++)
         {
@@ -228,155 +219,78 @@ public:
             table[cur.getI()]=now->getNext();
         }
         prev->setNext(now->getNext());
-        free(now);
+        delete(now);
         return  cur;
-
-
-
     }
-
-
-
-
-
 };
-
-
-
 void func()
 {
-    string line;
     ifstream myfile ("input.txt");
     SymbolTable st;
     bool first=true;
-
+    string command,info,value;
     if (myfile.is_open())
     {
-        while ( getline (myfile,line) )
+        while ( myfile.eof()==false )
         {
-            string temp1,temp2;
-            istringstream iss(line);
-            if(first)
-            {
-                int s;
-                iss>>s;
-                iss>>s;
-                st.setSize(s);
-                first=false;
-            }
-            for(;iss;)
-            {
-                iss>>temp1;
-                if(temp1.compare("I")==0)
+                if(first)
                 {
-
-                    string temp1,temp2;
-                    iss>>(temp1)>>temp2;
-
-
-                    SymbolInfo *si=new SymbolInfo(temp2,temp1);
-
-
-                    position pos=(position)st.insert(si);
+                    int s;
+                    myfile>>s;
+                    st.setSize(s);
+                    first=false;
+                }
+                myfile>>command;
+                if(command.compare("I")==0)
+                {
+                    myfile>>value>>info;
+                    SymbolInfo *si=new SymbolInfo(info,value);
+                    position pos=(position)(st.insert(si));
                     if(pos.getI()!=-1)
                     {
-                        cout<<"<"<<temp1<<","<<temp2<<">"<<"Inserted at position "<<(pos).getI()<<"  "<<pos.getJ()<<endl;
+                        cout<<"<"<<value<<","<<info<<">  "<<"Inserted at position "<<(pos).getI()<<" , "<<pos.getJ()<<endl;
                     }
                     else
                     {
-                        cout<<temp1<<" Already exist"<<endl;
+                         cout<<"<"<<value<<","<<info<<">   "<<"Already exist"<<endl;
                     }
-                    iss>>temp1;
                 }
-                else if(temp1.compare("P")==0)
+                else if(command.compare("P")==0)
                 {
-
                     st.print();
-                    iss>>temp1;
                 }
-                else if(temp1.compare("D")==0)
+                else if(command.compare("D")==0)
                 {
-                    iss>>temp1;
-
-                    position pos=(position)st.Delete(temp1);
+                    myfile>>value;
+                    position pos=(position)st.Delete(value);
                     if(pos.getI()==-1)
                     {
-                        cout<<temp1<<" Not Found"<<endl;
+                        cout<<value<<" Not Found"<<endl;
                     }
                     else
                     {
                         cout<<"Deleted from "<<(pos).getI()<<"  "<<pos.getJ()<<endl;
                     }
-                    iss>>temp1;
-
-
                 }
-                else if(temp1.compare("L")==0)
+                else if(command.compare("L")==0)
                 {
-                     iss>>temp1;
-
-                    position pos=(position)st.lookUp(temp1);
+                    myfile>>value;
+                    position pos=(position)st.lookUp(value);
                     if(pos.getI()==-1)
                     {
-                        cout<<temp1<<" Not Found"<<endl;
+                        cout<<value<<" Not Found"<<endl;
                     }
                     else
                     {
-                    cout<<"<"<<temp1<<","<<pos.getType()<<" Found at "<<(pos).getI()<<"  "<<pos.getJ()<<endl;
-                    iss>>temp1;
+                        cout<<"<"<<value<<","<<pos.getType()<<" Found at "<<(pos).getI()<<"  "<<pos.getJ()<<endl;
                     }
-
                 }
-
             }
-
-
-
-
-
         }
-        //st.print();
         myfile.close();
     }
-
-
-
-}
 int main()
 {
-
     func();
-   /* string s[8]={"asd","dsdef","qwe","dsdef","we","d78wrf","asq","zxcv"};
-
-    SymbolTable st(5);
-
-   for(int i=0;i<7;i=i+1)
-   {
-       string temp1,temp2;
-       temp1=s[i];
-       i++;
-       temp2=s[i];
-       SymbolInfo *si=new SymbolInfo(temp1,temp2);
-       cout<<(st.insert(si)).getI()<<endl;
-
-
-   }
-    st.print();*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return 0;
-
-
 }
